db46caecdf5092fea1f886e5f498f74257b0b7d5
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-28 00:59:12 +0530

    Add project files.

---
+++ Generic_Concept2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generic_Concept2
+{
+    class gen
+    {
+        public void check<T>(T a,T b)
+        {
+            bool x = a.Equals(b); // (A==B) COMPARSION OF DATA TYPE ID,INT AND A.EQUALS(); FOR OBJECT TYPE COMPARISION 
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            gen riccc = new gen();
+            riccc.check(22, 22);
+
+            riccc.check(22, 20);
+            riccc.check("RAM","RAM");
+
+
+
+        }
+    }
+}
